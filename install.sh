5ec452bb3de42e1bb94c6198101906df01dfb913
matteocrippa <matteocrippa@users.noreply.github.com>
2019-06-22 16:21:21 +0200

    stripped vars out

--- install.sh
+++ install.sh
@@ -1,20 +1,7 @@
 #!/bin/bash
 
 # env variables
-# feel free to edit according your needs
-
-export DISK=/dev/mmcblk0
-export LUKS=/dev/mapper/luks
-export SWAP_SIZE=6G
-export SWAP=swap
-export ROOT=root
-export VG=rootvg
-export HOSTNAME=moon
-export TIMEZONE=/Europe/Rome
-export USER=matteo
-export BOOT_CFG=/boot/loader/entries/arch.conf
-export HOOKS="HOOKS=(base systemd autodetect keyboard sd-vconsole modconf block sd-encrypt sd-lvm2 fsck filesystems)"
-
+. vars.sh
 
 # wipe
 wipe() {
